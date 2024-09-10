c4bf7935e7546f76f869012725be0b4fd5af6bab
Captain-Carl-Larc <155651529+Captain-Carl-Larc@users.noreply.github.com>
2024-09-10 17:10:15 +0300

    js file fixing

--- index.js
+++ index.js
@@ -1,10 +1,39 @@
+const saveBtn=document.getElementById("save-btn")
+const movieInputEl=document.getElementById("movie-input")
+const movieDisplayEl=document.getElementById("movie-ul")
+const errorP=document.getElementById("error-p")
+let moviesHolder=""
+let myMovies=["don","patty"]
+function saveMovie(){
+    if (movieInputEl.value !=="") {
+        const movie=movieInputEl.value
+        myMovies.push(movie)
+        movieInputEl.value=""   
+        console.log(myMovies) 
+        render(myMovies)
+    }
 
-
-
-
-
-
-
-const appSettings = {
-    databaseURL: "https://playground-c5b18-default-rtdb.europe-west1.firebasedatabase.app"
+    
 }
+
+function render(arr){
+    if(myMovies.length>0){
+        
+        for (let i = 0; i < arr.length; i++) {
+            moviesHolder +=`<li>${arr[i]}</li>`
+            
+        }
+        movieDisplayEl.innerHTML=moviesHolder
+    }else{
+        errorP.classList.remove("hidden")
+    }
+
+}
+
+saveBtn.addEventListener("click",saveMovie)
+
+
+
+
+
+
