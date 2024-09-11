face0c6192096d3d3e904218069391add804ea69
Captain-Carl-Larc <155651529+Captain-Carl-Larc@users.noreply.github.com>
2024-09-11 07:58:22 +0300

    complete application
    able to add movie and save it. next is make it usable as mobile app

--- index.js
+++ index.js
@@ -1,39 +1,58 @@
-const saveBtn=document.getElementById("save-btn")
-const movieInputEl=document.getElementById("movie-input")
-const movieDisplayEl=document.getElementById("movie-ul")
-const errorP=document.getElementById("error-p")
-let moviesHolder=""
-let myMovies=["don","patty"]
-function saveMovie(){
-    if (movieInputEl.value !=="") {
-        const movie=movieInputEl.value
-        myMovies.push(movie)
-        movieInputEl.value=""   
-        console.log(myMovies) 
-        render(myMovies)
-    }
+const movieInput=document.getElementById("movie")
+const saveBtn=document.getElementById("save")
+const deleteBtn=document.getElementById("delete")
+const displayUl=document.getElementById("display-ul")
+const erroP=document.getElementById("error-p")
 
-    
+let moviesFromLocStorage=JSON.parse(localStorage.getItem("myMovies"))
+let movies=[]
+
+//console.log("Movies from local storage at code start "+localStorage.getItem("myMovies"))
+
+if (moviesFromLocStorage) {
+    movies=moviesFromLocStorage
+    renderMovies(movies)
+}else{
+    document.getElementById("no-movie-error").classList.remove("hidden")
 }
 
-function render(arr){
-    if(myMovies.length>0){
-        
-        for (let i = 0; i < arr.length; i++) {
-            moviesHolder +=`<li>${arr[i]}</li>`
-            
-        }
-        movieDisplayEl.innerHTML=moviesHolder
+saveBtn.addEventListener("click",function(){
+    const movieTitle=movieInput.value
+    if (movieTitle) {
+        document.getElementById("no-movie-error").classList.add("hidden")
+        //erroP.classList.add("hidden")
+        movies.push(movieTitle)
+        movieInput.value=""
+        localStorage.setItem("myMovies",JSON.stringify(movies))
+        renderMovies(movies)
     }else{
-        errorP.classList.remove("hidden")
+        erroP.classList.remove("hidden")
+        setTimeout(function() {
+            //alert("3 seconds are over!")
+            erroP.classList.add("hidden")
+          }, 2000)
+        
     }
+})
 
+deleteBtn.addEventListener("dblclick",function(){
+    localStorage.clear()
+    renderMovies(movies)
+    window.location.reload();
+})
+renderMovies(movies) 
+
+function renderMovies(arr) {
+    let MovieListItems = ""
+    for (let i = 0; i < arr.length; i++) {
+        MovieListItems += `
+            <li>
+                <a target='_blank' href='${arr[i]}'>
+                    ${arr[i]}
+                </a>
+            </li>
+        `
+    }
+    displayUl.innerHTML = MovieListItems 
 }
-
-saveBtn.addEventListener("click",saveMovie)
-
-
-
-
-
 
